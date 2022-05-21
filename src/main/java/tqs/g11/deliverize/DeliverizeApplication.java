a385f30604a31f8a2d3dbd34715aa365ef088c7b
inryatt <cffonseca@ua.pt>
2022-05-21 11:59:55 +0100

    Initialized Spring Project

---
+++ src/main/java/tqs/g11/deliverize/DeliverizeApplication.java
@@ -0,0 +1,13 @@
+package tqs.g11.deliverize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliverizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliverizeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tqs/g11/deliverize/DeliverizeApplicationTests.java
@@ -0,0 +1,13 @@
+package tqs.g11.deliverize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliverizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
